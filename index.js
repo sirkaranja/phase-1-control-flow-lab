100c6d49343cf22391a24c083981f3aed78b9b7a
Daniel Karanja <sirkaranja12@gmail.com>
2023-03-17 00:01:28 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
+  
+  if(distance<=400){
+    return "This one is on me!";
+  }
+  else if(distance > 2000 && distance <=2500){
+    return "I will gladly take your thirty bucks."
+  
+  
+  }
+  else if(distance > 2500){
+    return "No can do."
+  }
+
+  console.log(scuberGreetingForFeet(3000));
+ 
+ 
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+let destination;
+function ternaryCheckCity(destination) {
+  return destination === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
+console.log(ternaryCheckCity(destination));
 
 function switchOnCharmFromTip(){
   // Write your code here!
 }
+// function switchOnCharmFromTip()
+
+let tip;
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
+console.log(switchOnCharmFromTip(tip));
